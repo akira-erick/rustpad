fcd581a3adc4429074dc1766e3229d8e66ca3d1f
akira-erick <kajitani.motta@gmail.com>
2025-05-12 19:34:13 -0300

    feat(pad): first endpoint test

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,25 @@
-fn main() {
-    
+use actix_web::{web, App, HttpServer, Responder, HttpResponse};
+use serde::{Deserialize, Serialize};
+
+#[derive(Serialize, Deserialize)]
+struct Content {
+    body: String,
+}
+
+async fn get_content() -> impl Responder {
+    let content = Content {
+        body: "Hello, world!".to_string(),
+    };
+    HttpResponse::Ok().json(content)
+}
+
+#[actix_web::main]
+async fn main() -> std::io::Result<()> {
+    HttpServer::new(|| {
+        App::new()
+            .route("/rust/", web::get().to(get_content))
+    })
+    .bind("127.0.0.1:8080")?
+    .run()
+    .await
 }
