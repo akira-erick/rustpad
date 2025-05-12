638c38bd42c60412e5da7ae9eb4ce67014337c30
akira-erick <kajitani.motta@gmail.com>
2025-05-12 19:47:45 -0300

    feat(pad): make actual endpoint to get values

--- src/main.rs
+++ src/main.rs
@@ -1,23 +1,41 @@
 use actix_web::{web, App, HttpServer, Responder, HttpResponse};
 use serde::{Deserialize, Serialize};
+use std::sync::Mutex;
+use actix_web::web::Data;
+use std::collections::HashMap;
 
 #[derive(Serialize, Deserialize)]
 struct Content {
     body: String,
 }
 
-async fn get_content() -> impl Responder {
-    let content = Content {
-        body: "Hello, world!".to_string(),
-    };
-    HttpResponse::Ok().json(content)
+struct AppState {
+    store: Mutex<HashMap<String, Content>>,
+}
+
+async fn get_content(path: web::Path<String>, data: Data<AppState>) -> impl Responder {
+    let path = path.into_inner();
+    let store = data.store.lock().unwrap();
+    if let Some(content) = store.get(&path) {
+        HttpResponse::Ok().json(content)
+    } else {
+        HttpResponse::Ok().json(Content {
+            body: "".to_string(),
+        })
+    }
 }
 
 #[actix_web::main]
 async fn main() -> std::io::Result<()> {
-    HttpServer::new(|| {
+
+    let app_state = web::Data::new(AppState {
+        store: Mutex::new(HashMap::new()),
+    });
+
+    HttpServer::new(move || {
         App::new()
-            .route("/rust/", web::get().to(get_content))
+            .app_data(app_state.clone())
+            .route("/rust/{path}", web::get().to(get_content))
     })
     .bind("127.0.0.1:8080")?
     .run()
