ab9338e8cc3413665d410483abc1aa9bf8e437a9
akira-erick <kajitani.motta@gmail.com>
2025-05-12 19:26:21 -0300

    changing everthing

--- src/main.rs
+++ src/main.rs
@@ -1,17 +1,3 @@
-mod content;
-mod pad_map_persistece;
-mod pad_persistence_trait;
-mod pad_service;
-
-#[macro_use]
-extern crate rocket;
-
-#[get("/")]
-fn index() -> &'static str {
-    "Hello, world!"
-}
-
-#[launch]
-fn rocket() -> _ {
-    rocket::build().mount("/", routes![index])
+fn main() {
+    
 }
--- src/pad_persistence_trait.rs
+++
@@ -1,5 +0,0 @@
-#[allow(dead_code)]
-pub trait PadPersistenceTrait {
-    fn get(&self, key: &str) -> Result<String, String>;
-    fn set(&mut self, key: &str, value: String) -> Result<(), String>;
-}
